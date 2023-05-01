4ad55ab97a6c5161e9fc03565995c40cfc432864
YongJun Kwon <dydwns18011@gmail.com>
2023-05-01 16:33:00 +0900

    feat: 검색 기능 컴포넌트 추가

---
+++ src/components/Search.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import  "../styles/SearchWebtoon.css"
+export function SearchWebtoon(){
+
+    return(
+        <div>
+            <div className="SearchBar__search_area--L61RY">
+                <input type = "text" className="SearchBar__search_input--k5nfk"/>
+                <button className="SearchBar__btn_search--SsL7v">검색</button>
+            </div>
+        </div>
+    );
+}
