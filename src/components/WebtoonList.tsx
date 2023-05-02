c75402f290210828ca83970c9fd49c5b9bc9f50b
YongJun Kwon <dydwns18011@gmail.com>
2023-05-02 23:11:51 +0900

    feat: 웹툰 목록 정보 화면

--- src/components/WebtoonList.tsx
+++ src/components/WebtoonList.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { RootState } from "../features/store";
-
+import { webtoonList } from "../features/webtoon/webtoonSlice"
 export function WebtoonList(){
-    const webtoonList = useSelector((state:RootState) => (state.webtoon.webtoonList))
-
+    const list: Array<any> = useSelector(webtoonList);
+    console.log(list);
     return(
         <div>
-            {webtoonList}
+            {
+                list.map((data,key)=>(
+                    <div>
+                        <li> 작가 : {data.author}</li>
+                        <li> 제목 : {data.title}</li>
+                        <li> 도메인 : {data.service}</li>
+                    </div>
+                ))
+            }
         </div>
     )
 }
