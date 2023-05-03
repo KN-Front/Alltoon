79d74ecb1e086ae1100172659ea078f6af35f42a
YongJun Kwon <dydwns18011@gmail.com>
2023-05-03 22:26:00 +0900

    feat: react router dom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Main } from './components/Main';
+import Main  from './pages/MainPage';
+import SearchPage  from './pages/SearchPage'
+import Nav from './components/Nav';
+import { Routes, Route, Outlet} from "react-router-dom"
+
 import './App.css';
+
+const Layout = () =>{
+  return(
+    <div>
+      <Nav/>
+      <Outlet/>
+    </div>
+    
+
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+        <Routes>
+          <Route path = "/" element ={<Layout/>}/>
+          <Route index element = {<Main/>}/>
+          <Route path='search' element = {<SearchPage/>}/>
+        </Routes>
         <Main />
     </div>
   );
