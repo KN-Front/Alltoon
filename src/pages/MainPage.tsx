cfd3a0d7c8cdabb209880819b7e482259b2045ef
YongJun Kwon <dydwns18011@gmail.com>
2023-05-03 22:15:49 +0900

    style: 폴더 구조 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Search } from '../../components/nav/Search';
-import { WeekList } from '../../components/WeeksList';
-import { WebtoonList } from '../../components/WebtoonList';
-import { useAppSelector, useAppDispatch } from '../../features/hooks';
-import {fetchWebtoonList} from '../../features/webtoon/webtoonActions'
+import { Search } from '../components/Search';
+import { WeekList } from '../components/WeeksList';
+import { WebtoonList } from '../components/WebtoonList';
+import { useAppSelector, useAppDispatch } from '../features/hooks';
+import {fetchWebtoonList} from '../features/webtoon/webtoonActions'
 export function Main() {
 
   const dispatch = useAppDispatch();
