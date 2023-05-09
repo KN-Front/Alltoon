7cf09619bef9415ba11ad896063aed95af44b804
YongJun Kwon <dydwns18011@gmail.com>
2023-05-09 22:02:47 +0900

    fix: 요일 검색기능 오류수정

--- src/components/WeeksList.tsx
+++ src/components/WeeksList.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { MouseEventHandler, useState } from "react";
 import { useSelector } from "react-redux";
 import { weeksList } from "../features/webtoon/webtoonSlice";
+import { fetchWebtoonList} from "../features/webtoon/webtoonActions"
+import { useAppDispatch } from "../features/hooks"
 import "../styles/WeekList.css"
-
+/**
+ * 요일 목록 컴포넌트
+ * @returns 
+ */
 export function WeekList(){
-    const weeks = useSelector(weeksList)
+    const dispatch = useAppDispatch();
+    const weeks = useSelector(weeksList);
+    const [selectedWeek, setSelectedWeek] = useState('');
+
+    const searchWebtoon = (week: string) =>{
+      setSelectedWeek(week);
+      let param = {
+        service : "naver",
+        updateDay : selectedWeek
+      }
+      dispatch(fetchWebtoonList(param))
+
+    }
+
     return(
     <div>
         <nav>
           <ul className="SubNavigationBar__snb_list--tAZvu">    
-              {weeks.map((item, index) => (
-                  <li key={index} className="SubNavigationBar__item--tmE0E">
-                    <a className="SubNavigationBar__link--PXX5B">{item.value}</a>
-                  </li>
+              {weeks.map((item) => (
+                  <button key={item.key} onClick={()=> searchWebtoon(item.key)}>{item.value}</button>
               ))}
           </ul>  
         </nav> 
