ce2409aba207977bbc66ee93a06d1035e6302c7e
YongJun Kwon <dydwns18011@gmail.com>
2023-05-14 15:55:59 +0900

    refactor: 검색 페이지 리팩터링

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,32 +1,36 @@
-import React , {useEffect, useState} from 'react'
+import React from 'react'
 import {searchList} from '../features/webtoon/webtoonSlice'
 import {useSelector} from 'react-redux'
 const SearchPage = () =>{
     
-
     const list: Array<any> = useSelector(searchList);
 
-    const test = () =>{
-        console.log(list);
+    if(list.length){
+        return(
+            <div>
+                <button></button>
+                 { list.map((data)=>(
+                        <div key={data._id}>
+                            title : {data.title}
+                            img : {data.img}
+                            author : {data.author}
+                            service : {data.service}
+                            url : {data.url}
+                            updateDays : {data.updateDays}
+                            fanCount : {data.fanCount}
+                        </div>
+                    )) 
+                 }
+            </div>
+        )
+    }else{
+        return(
+            <div>
+                값이 없습니다.
+            </div>
+        )
     }
-    return(
-        <div>
-            <button onClick={()=>test()}></button>
-             {
-                list.map((data)=>(
-                    <div key={data._id}>
-                        title : {data.title}
-                        img : {data.img}
-                        author : {data.author}
-                        service : {data.service}
-                        url : {data.url}
-                        updateDays : {data.updateDays}
-                        fanCount : {data.fanCount}
-                    </div>
-                ))
-             }
-        </div>
-    )
+    
 }
 
 export default SearchPage
