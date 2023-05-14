597dbc25dd39cc1c17856f0f0110af8a03aa473a
YongJun Kwon <dydwns18011@gmail.com>
2023-05-14 15:56:23 +0900

    style: 웹툰 목록 컴포넌트 스타일 추가

--- src/components/WebtoonList.tsx
+++ src/components/WebtoonList.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import '../styles/WebtoonList.css'
 import { useSelector } from "react-redux";
-import { webtoonList } from "../features/webtoon/webtoonSlice"
+import { webtoonList,searchParam } from "../features/webtoon/webtoonSlice"
 
 /**
  * 웹툰 목록 컴포넌트
@@ -8,17 +9,54 @@ import { webtoonList } from "../features/webtoon/webtoonSlice"
  */
 export function WebtoonList(){
     const list: Array<any> = useSelector(webtoonList);
+    const selectedWeek = useSelector(searchParam).updateDay;
+
+    const getWeek = () =>{
+        if(selectedWeek == 'mon'){
+            return '월'
+        }else if(selectedWeek == 'tue'){
+            return '화'
+        }else if(selectedWeek == 'wed'){
+            return '수'
+        }else if(selectedWeek == 'thu'){
+            return '목'
+        }else if(selectedWeek == 'fri'){
+            return '금'
+        }else if(selectedWeek == 'sat'){
+            return '토'
+        }else if(selectedWeek == 'sun '){
+            return '일'
+        }
+    }
+
     return(
         <div>
+            <div className="ComponentHead__component_head--O2tPr">
+                <div className="ComponentHead__title_area--IEQEG">
+                    <h2 className="ComponentHead__title--TjYVo">
+                        <span className="ComponentHead__text--dhKW7"> {getWeek()}요일 전체웹툰</span>
+                    </h2>
+                </div>
+            </div>
+            <ul className="ContentList__content_list--q5KXY">
             {
                 list.map((data,key)=>(
-                    <div key={key}>
-                        <li> 작가 : {data.author}</li>
-                        <li> 제목 : {data.title}</li>
-                        <li> 도메인 : {data.service}</li>
-                    </div>
-                ))
-            }
+                    <li key={key} className="item">
+                        <a href={data.url}>
+                            <div className="Poster__thumbnail_area--gviWY Poster__type160x207--EQAM8">
+                                <div className="Poster__bullet_wrap--VcWFJ"></div>
+                                <img src={data.img} alt={data.title} className="Poster__image--d9XTI"></img>
+                            </div>
+                        </a>
+                        <div>
+                            {data.title}
+                            {data.author}
+                            {data.service}
+                            {data.fanCount}
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
