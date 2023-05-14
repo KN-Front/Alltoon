d0fc9dea628ddbec6b7cafccce55b5758251017b
YongJun Kwon <dydwns18011@gmail.com>
2023-05-14 15:53:32 +0900

    feat: nav 컴포넌트 메인 이동 및 스타일 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,33 @@
 import { Search } from "./Search";
+import {useNavigate, useLocation} from "react-router-dom"
+import '../styles/Nav.css'
 /**
  * Nav 컴포넌트
  * @returns 
  */
 const Nav = () =>{
+
+    const navigate = useNavigate();
+    const loaction = useLocation().pathname;
+    /**
+     * main 이동
+     */
+    const moveMain = () => {
+        if(loaction !== '/'){
+            navigate('/');
+        }
+    }
+
     return(
-        <div>
+        <div className="BrandBar__gnb_wrap--Nh2jt">
+            <h1 className="BrandBar__logo_area--Y_7dM">
+                <a className="BrandBar__logo_naver--srvE0" onClick={()=>moveMain()}>
+                    <span className="blind"> 웹툰</span>
+                </a>
+                <div >
+                </div>
+            </h1>
+            
             <Search/>
         </div>
     )
