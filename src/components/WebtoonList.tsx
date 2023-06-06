1747f150d412e92bdcd8ca10e8c7b9473baf2cdd
YongJun Kwon <dydwns18011@gmail.com>
2023-06-06 23:59:40 +0900

    feat: 스크롤 이벤트 설정

--- src/components/WebtoonList.tsx
+++ src/components/WebtoonList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import '../styles/WebtoonList.css'
 import { useSelector } from "react-redux";
 import { webtoonList,searchParam } from "../features/webtoon/webtoonSlice"
@@ -10,6 +10,32 @@ import { webtoonList,searchParam } from "../features/webtoon/webtoonSlice"
 export function WebtoonList(){
     const list: Array<any> = useSelector(webtoonList);
     const selectedWeek = useSelector(searchParam).updateDay;
+    const scrollRef = useRef<any>(null);
+
+    useEffect(() => {
+        const scrollContainer = scrollRef.current;
+    
+        const handleScroll = () => {
+          const isScrolledToBottom =
+            scrollContainer.scrollHeight - scrollContainer.scrollTop ===
+            scrollContainer.clientHeight;
+    
+          if (isScrolledToBottom) {
+            handleScrollToBottom();
+          }
+        };
+    
+        scrollContainer.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          scrollContainer.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+      const handleScrollToBottom = () => {
+        // todo 웹툰 정보 가져오기
+      };
+
 
     return(
         <div>
@@ -20,34 +46,43 @@ export function WebtoonList(){
                     </h2>
                 </div>
             </div>
-            <ul className="ContentList__content_list--q5KXY">
-            {
-                list?.map((data,key)=>(
-                    <li key={key} className="item">
-                        <a href={data.url}>
-                            <div className="Poster__thumbnail_area--gviWY Poster__type160x207--EQAM8">
-                                <div className="Poster__bullet_wrap--VcWFJ"></div>
-                                <img src={data.img} alt={data.title} className="Poster__image--d9XTI"></img>
-                            </div>
-                        </a>
-                        <div className="ContentList__info_area--bXx7h">
-                            <a className="ContentTitle__title_area--x24vt" href={data.url}>
-                                <span className="ContentTitle__title--e3qXt">
-                                    <span className="text">
-                                      {data.title}
-                                    </span>
-                                </span>
+            <div ref={scrollRef}
+                style={{
+                    height: '700px',
+                    overflow: 'auto',
+                }}
+                className="webtoonListScroll"
+                >
+                <ul className="ContentList__content_list--q5KXY">
+                {
+                    list?.map((data,key)=>(
+                        <li key={key} className="item">
+                            <a href={data.url}>
+                                <div className="Poster__thumbnail_area--gviWY Poster__type160x207--EQAM8">
+                                    <div className="Poster__bullet_wrap--VcWFJ"></div>
+                                    <img src={data.img} alt={data.title} className="Poster__image--d9XTI"></img>
+                                </div>
                             </a>
-                            <div className="ContentAuthor__author_wrap--fV7Lo">
-                                <a className="ContentAuthor__author--CTAAP">
-                                 {data.author}
+                            <div className="ContentList__info_area--bXx7h">
+                                <a className="ContentTitle__title_area--x24vt" href={data.url}>
+                                    <span className="ContentTitle__title--e3qXt">
+                                        <span className="text">
+                                        {data.title}
+                                        </span>
+                                    </span>
                                 </a>
+                                <div className="ContentAuthor__author_wrap--fV7Lo">
+                                    <a className="ContentAuthor__author--CTAAP">
+                                    {data.author}
+                                    </a>
+                                </div>
+                                {/* {data.fanCount} */}
                             </div>
-                            {/* {data.fanCount} */}
-                        </div>
-                    </li>
-                ))}
-            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            
         </div>
     )
 }
