e785826b7ff9200aba413ad2968d87467942f79c
YongJun Kwon <dydwns18011@gmail.com>
2023-06-07 00:03:36 +0900

    fix: 404 error 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,28 +1,26 @@
-import React, { useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { WeekList } from '../components/WeeksList';
 import { WebtoonList } from '../components/WebtoonList';
 import { useAppDispatch } from '../features/hooks';
 import {fetchWebtoonList} from '../features/webtoon/webtoonActions'
-import { searchParam } from '../features/webtoon/webtoonSlice'
+import { searchParam, webtoonActions } from '../features/webtoon/webtoonSlice'
+import {getWebtoonInfo} from '../common/api/webtoonAPI'
 import { useSelector } from "react-redux";
 
 const Main = () => {
 
+  let [list, setList] = useState<any>();
   const dispatch = useAppDispatch();
   const param = useSelector(searchParam)
   useEffect(()=>{
     dispatch(fetchWebtoonList(param))
   },[]);
 
-
   return (
     <div>
         <hr/>
 
       <div>
-        <div>
-            슬라이드 
-        </div>
         <hr/>
 
         <div>
