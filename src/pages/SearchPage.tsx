189b5ef4272581effe1b348b7426fb893f96d2bd
kwonyongjun <dydwns18011@gmail.com>
2024-01-17 12:50:42 +0900

    ️♻ Refactor: SearchPage 컴포넌트 분리

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,35 +1,11 @@
 import React from 'react'
-import { searchWebtoon } from '../features/webtoon/webtoonSlice'
-import { useSelector } from 'react-redux'
-import { webtoonInfo } from '@/types'
+import SearchList from '../components/SearchList'
 const SearchPage = () =>{
     
-    const webtoon:webtoonInfo = useSelector(searchWebtoon);
 
-    if(webtoon.webtoons.length){
-        return(
-            <div>
-                 { webtoon.webtoons.map((data)=>(
-                        <div key={data._id}>
-                            title : {data.title}
-                            img : {data.img}
-                            author : {data.author}
-                            service : {data.service}
-                            url : {data.url}
-                            updateDays : {data.updateDays}
-                            fanCount : {data.fanCount}
-                        </div>
-                    )) 
-                 }
-            </div>
-        )
-    }else{
-        return(
-            <div>
-                값이 없습니다.
-            </div>
-        )
-    }
+    return(
+        <SearchList/>
+    )
     
 }
 
