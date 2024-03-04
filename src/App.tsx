b747d8bbf8f08e4b4e7e60b634899408b2e29792
kwonyongjun <dydwns18011@gmail.com>
2024-03-04 14:23:09 +0900

    remove : test 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Main from './components/test/Main';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import Main from './pages/MainPage';
 import SearchPage from './pages/SearchPage';
 import Header from './components/Heaer';
 import Loading from './components/Loading';
-import { Routes, Route, Outlet } from 'react-router-dom';
-// import './styles/webtoon.css';
-import './styles/test/css/main.css';
+import './styles/webtoon.css';
 import './App.css';
 
 const Layout = () => {
@@ -20,15 +19,13 @@ const Layout = () => {
 function App() {
     return (
         <div className="App">
-            {/* <Loading/>
-      <Routes>
-        <Route path = "/" element ={<Layout/>}>
-          <Route index element = {<Main/>}/>
-          <Route path='search' element = {<SearchPage/>}/>
-        </Route>
-      </Routes> */}
-
-            <Main />
+            <Loading />
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Main />} />
+                    <Route path="search" element={<SearchPage />} />
+                </Route>
+            </Routes>
         </div>
     );
 }
