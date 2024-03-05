8ea1af974554943cdccc3850ba1cf6f45b3c072c
kwonyongjun <dydwns18011@gmail.com>
2024-03-05 19:24:15 +0900

    style : 검색 페이지 UI 변경

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,179 +1,74 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { searchWebtoon } from '@/features/webtoon/webtoonSlice';
-import { webtoonInfo,webtoons } from '@/types';
+import { webtoonInfo, webtoons } from '@/types';
 import { getUpdateWeekArrayToString } from '@/common/utill/week';
-import {AdultIcon, NewIcon, ResetIcon, UpIcon} from './icon';
+import { AdultIcon, NewIcon, ResetIcon, UpIcon } from './icon';
+import { loading } from '@/features/webtoon/webtoonSlice';
+import WebtoonLoading from './WebtoonLoading';
 
-const SearchList = () =>{
-    const webtoon:webtoonInfo = useSelector(searchWebtoon);
-    const[naverWebtoon, setNaverWebtoon] = useState<webtoons[]>([]);
-    const[kakaoWebtoon, setKakaoWebtoon] = useState<webtoons[]>([]);
-    const[kakaoPageWebtoon, setKakaoPageWebtoon] = useState<webtoons[]>([]);
-    
-    useEffect(()=>{
-        clear()
-            .then(filterByService);
-    },[webtoon]);
+const SearchList = () => {
+    const webtoon: webtoonInfo = useSelector(searchWebtoon);
+    const [naverWebtoon, setNaverWebtoon] = useState<webtoons[]>([]);
+    const [kakaoWebtoon, setKakaoWebtoon] = useState<webtoons[]>([]);
+    const [kakaoPageWebtoon, setKakaoPageWebtoon] = useState<webtoons[]>([]);
+    const isLoading: boolean = useSelector(loading);
+    useEffect(() => {
+        clear().then(filterByService);
+    }, [webtoon]);
 
-    const clear = async() =>{
+    const clear = async () => {
         setNaverWebtoon([]);
         setKakaoWebtoon([]);
         setKakaoPageWebtoon([]);
-    }
-    const filterByService = async ()=>{
-        webtoon.webtoons.forEach((webtoon)=>{
-            if(webtoon.service === 'naver'){
+    };
+    const filterByService = async () => {
+        webtoon.webtoons.forEach((webtoon) => {
+            if (webtoon.service === 'naver') {
                 setNaverWebtoon((prev) => [...prev, webtoon]);
-            }else if(webtoon.service === 'kakao'){
+            } else if (webtoon.service === 'kakao') {
                 setKakaoWebtoon((prev) => [...prev, webtoon]);
-            }else if(webtoon.service === 'kakaoPage'){
+            } else if (webtoon.service === 'kakaoPage') {
                 setKakaoPageWebtoon((prev) => [...prev, webtoon]);
             }
-        })
-    }
+        });
+    };
 
-    const sliceText = (text:string) =>{
+    const sliceText = (text: string) => {
         return `${text.slice(0, 35)}...`;
-    }
+    };
 
-    return(
-        <div className='searchList'>
-            <div className='serviceTitle'>
-                <div className='serviceSub'>
-                    <h2>Naver</h2>
-                    <p>{naverWebtoon.length} 개</p>
+    return (
+        <div className="w-full">
+            {isLoading ? (
+                <WebtoonLoading />
+            ) : (
+                <div className="w-full">
+                    <div id="body" className="grid grid-cols-2 lg:grid-cols-6 xl:grid-cols-6 2xl:grid-cols-6 gap-4 p-4">
+                        {webtoon.webtoons.map((data, key) => (
+                            <div key={key}>
+                                <article>
+                                    <div className="webtoonBox">
+                                        <header>
+                                            <a href={data.url}>
+                                                <img src={data.img} alt={data.title}></img>
+                                            </a>
+                                        </header>
+                                        <div>
+                                            <a href={data.url}>
+                                                <span>{data.title}</span>
+                                            </a>
+                                            <p>{data.author}</p>
+                                        </div>
+                                    </div>
+                                </article>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>    
-            <div className='wrap'>
-            { Boolean(naverWebtoon) ? 
-            naverWebtoon.map((webtoon)=>(
-                <ul className='searchWebtoon' key={webtoon._id}>
-                    <li>
-                        <a href={webtoon.url}>
-                            <div className='poster'>
-                                <img src={webtoon.img}></img>                           
-                            </div>
-                        </a> 
-                        <div className='info'>
-                            <div className='additional'>
-                                {webtoon.additional.adult ? 
-                                <AdultIcon/> : null}
-                                {webtoon.additional.new ? 
-                                <NewIcon/> : null}
-                                {webtoon.additional.up ? 
-                                <UpIcon/> : null}
-                                {webtoon.additional.rest ? 
-                                <ResetIcon/> : null}
-                            </div>
-                            <a href={webtoon.url}>
-                                <span>{webtoon.title}</span>
-                            </a>
-                            <div className='author'>
-                                <span>작가 : {sliceText(webtoon.author)}</span>
-                            </div>
-                            <div className='updateDay'>
-                                <span>업데이트 날짜 : {getUpdateWeekArrayToString(webtoon.updateDays)}</span>
-                            </div>
-                            
-                        </div>
-                    </li>
-                </ul>
-            )) : 
-                <label>
-                    결과가 없습니다.
-                </label>
-            }
-            </div>
-            <div className='serviceTitle'>
-                <div className='serviceSub'>
-                    <h2>Kakao</h2>
-                    <p>{kakaoWebtoon.length} 개</p>
-                </div>
-            </div>
-            <div className='wrap'>
-            { Boolean(kakaoWebtoon) ? 
-            kakaoWebtoon.map((webtoon,index)=>(
-                <ul className='searchWebtoon' key={webtoon._id}>
-                    <li>
-                        <a href={webtoon.url}>
-                            <div className='poster'>
-                                <img src={webtoon.img}></img>                           
-                            </div>
-                        </a> 
-                        <div className='info'>
-                            <div className='additional'>
-                                {webtoon.additional.adult ? 
-                                <AdultIcon/> : null}
-                                {webtoon.additional.new ? 
-                                <NewIcon/> : null}
-                                {webtoon.additional.up ? 
-                                <UpIcon/> : null}
-                                {webtoon.additional.rest ? 
-                                <ResetIcon/> : null}
-                            </div>
-                            <a href={webtoon.url}>
-                                <span>{webtoon.title}</span>
-                            </a>
-                            <div className='author'>
-                                <span>작가 : {sliceText(webtoon.author)}</span>
-                            </div>
-                            <div className='updateDay'>
-                                <span>업데이트 날짜 : {getUpdateWeekArrayToString(webtoon.updateDays)}</span>
-                            </div>
-                            
-                        </div>
-                    </li>
-                </ul>
-            )) : undefined
-            }
-            </div>
-            <div className='serviceTitle'>
-                <div className='serviceSub'>
-                    <h2>KakaoPage</h2>
-                    <p>{kakaoPageWebtoon.length} 개</p>
-                </div>
-            </div>
-
-            <div className='wrap'>
-            { Boolean(kakaoPageWebtoon) ? 
-            kakaoPageWebtoon.map((webtoon, index)=>(
-                <ul className='searchWebtoon' key={webtoon._id}>
-                    <li>
-                        <a href={webtoon.url}>
-                            <div className='poster'>
-                                <img src={webtoon.img}></img>                           
-                            </div>
-                        </a> 
-                        <div className='info'>
-                            <div className='additional'>
-                                {webtoon.additional.adult ? 
-                                <AdultIcon/> : null}
-                                {webtoon.additional.new ? 
-                                <NewIcon/> : null}
-                                {webtoon.additional.up ? 
-                                <UpIcon/> : null}
-                                {webtoon.additional.rest ? 
-                                <ResetIcon/> : null}
-                            </div>
-                            <a href={webtoon.url}>
-                                <span>{webtoon.title}</span>
-                            </a>
-                            <div className='author'>
-                                <span>작가 : {sliceText(webtoon.author)}</span>
-                            </div>
-                            <div className='updateDay'>
-                                <span>업데이트 날짜 : {getUpdateWeekArrayToString(webtoon.updateDays)}</span>
-                            </div>
-                            
-                        </div>
-                    </li>
-                </ul>
-            )) : undefined
-            }
-            </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
-export default SearchList
+export default SearchList;
