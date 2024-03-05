692ea5020e3e737cb3959f2eb9c6249d2b98db55
kwonyongjun <dydwns18011@gmail.com>
2024-03-05 19:25:13 +0900

    style : Search 페이지 UI 변경

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import SearchList from '@/components/SearchList'
+import React from 'react';
+import SearchList from '@/components/SearchList';
+import { Search } from '@/components/Search';
+import Sidebar from '@/components/Sidebar';
 
-const SearchPage = () =>{
+const SearchPage = () => {
+    return (
+        <div className="lg:px-24 mx-auto justify-center lg:mt-8 pt-10">
+            <Search />
+            {/* <SearchList /> */}
+            <div className="body mt-4 flex flew-row space-x-4">
+                <Sidebar />
+                <SearchList />
+            </div>
+        </div>
+    );
+};
 
-    return(
-        <SearchList/>
-    )
-    
-}
-
-export default SearchPage
+export default SearchPage;
