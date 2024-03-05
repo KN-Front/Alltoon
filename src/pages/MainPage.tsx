13b92f08a07593809e9ef93d2fe7706908c8af1b
kwonyongjun <dydwns18011@gmail.com>
2024-03-05 19:25:23 +0900

    style : Main 페이지 UI 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,22 +1,21 @@
-import React, {useState, useEffect} from 'react';
-import { WeekList } from '@/components/WeeksList';
+import React, { useState, useEffect } from 'react';
+import { Weeks } from '@/components/Weeks';
 import { WebtoonList } from '@/components/WebtoonList';
 import { useAppDispatch } from '@/features/hooks';
-import {fetchWebtoonList} from '@/features/webtoon/webtoonActions'
-
+import { fetchWebtoonList } from '@/features/webtoon/webtoonActions';
 
 const Main = () => {
-  const dispatch = useAppDispatch();
-  useEffect(()=>{
-    dispatch(fetchWebtoonList())
-  },[]);
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        dispatch(fetchWebtoonList());
+    }, []);
 
-  return (
-    <main>
-          <WeekList/>
-          <WebtoonList/>
-    </main>
-  );
-}
+    return (
+        <div className="lg:px-24 mx-auto justify-center lg:mt-8 pt-10">
+            <Weeks />
+            <WebtoonList />
+        </div>
+    );
+};
 
 export default Main;
