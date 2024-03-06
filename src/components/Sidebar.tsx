6f2f63c8713598e28dcad783c8e29170f0ca8eb6
kwonyongjun <dydwns18011@gmail.com>
2024-03-06 13:52:39 +0900

    style : 배경색 변경

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,24 +8,29 @@ const Sidebar = () => {
                     <div
                         className={`bg-zinc-700/30 justify-between button-animate p-2 px-3 text-sm flex cursor-pointer mt-2 rounded-sm hover:bg-zinc-700/20`}
                     >
-                        <p className="text-center">Series</p>
+                        <p className="text-center">All</p>
                         <p className="text-center text-sm">3</p>
                     </div>
                 </div>
-                <div className="mt-3 px-1">
-                    <div className="bg-zinc-700/40 w-full h-[1px]"></div>
+                <div className="flex flex-col">
+                    <div
+                        className={`bg-zinc-700/10 justify-between button-animate p-2 px-3 text-sm flex cursor-pointer mt-2 rounded-sm hover:bg-zinc-700/20`}
+                    >
+                        <p className="text-center">Naver</p>
+                        <p className="text-center text-sm">3</p>
+                    </div>
                 </div>
                 <div className="flex flex-col mt-1 overflow-auto">
                     <div
-                        className={`bg-zinc-700/30 justify-between button-animate p-2 px-3 text-sm flex cursor-pointer mt-2 rounded-sm hover:bg-zinc-700/20`}
+                        className={`bg-zinc-700/10 justify-between button-animate p-2 px-3 text-sm flex cursor-pointer mt-2 rounded-sm hover:bg-zinc-700/20`}
                     >
-                        <p className="text-center">Comedy</p>
+                        <p className="text-center">KaKao</p>
                         <p className="text-center text-sm">2</p>
                     </div>
                     <div
                         className={`bg-zinc-700/10 justify-between button-animate p-2 px-3 text-sm flex cursor-pointer mt-2 rounded-sm hover:bg-zinc-700/20`}
                     >
-                        <p className="text-center">Action</p>
+                        <p className="text-center">KaKaoPage</p>
                         <p className="text-center text-sm">3</p>
                     </div>
                 </div>
