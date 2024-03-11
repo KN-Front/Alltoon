14f344c6352b2e88678ea7507498dd36d779c9aa
kwonyongjun <dydwns18011@gmail.com>
2024-03-11 13:22:30 +0900

    style : active 적용

--- src/components/Weeks.tsx
+++ src/components/Weeks.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { weeksList, serviceList, webtoonActions, searchParam, loading } from '@/features/webtoon/webtoonSlice';
+import { weeks as weekList, serviceList, webtoonActions, searchParam, loading } from '@/features/webtoon/webtoonSlice';
 import { fetchWebtoonList } from '@/features/webtoon/webtoonActions';
 import { useAppDispatch } from '@/features/hooks';
 import { week } from '@/types';
@@ -10,7 +10,7 @@ import { week } from '@/types';
  */
 export function Weeks() {
     const dispatch = useAppDispatch();
-    const weeks = useSelector(weeksList);
+    const weeks = useSelector(weekList);
     const service = useSelector(serviceList);
     const param = useSelector(searchParam);
 
@@ -30,27 +30,15 @@ export function Weeks() {
         <div className="col-span-8 space-x-8 lg:mt-0 mt-4 justify-left flex items-center text-center mx-auto h-24 p-14">
             {weeks.map((item) => (
                 <div
-                    className="relative text-gray-400  font-medium transition-all duration-200 hover:text-white cursor-pointer"
+                    className={`relative text-gray-400  font-medium transition-all duration-200 hover:text-white cursor-pointer ${
+                        item.key == param.updateDay ? 'text-white ' : ''
+                    }`}
                     key={item.key}
                     onClick={() => getWeekParam(item)}
-                    // className={`${item.key == param.updateDay ? 'active' : ''}`}
                 >
                     {item.key}
                 </div>
             ))}
         </div>
-        // <div className="weekList">
-        //     <ul>
-
-        //     </ul>
-        //     <select onChange={getServiceParam}>
-        //         {service.map((item, index) => (
-        //             <option key={index} value={item.toString()}>
-        //                 {' '}
-        //                 {item}
-        //             </option>
-        //         ))}
-        //     </select>
-        // </div>
     );
 }
