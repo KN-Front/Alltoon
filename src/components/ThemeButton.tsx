008c7dd450ddfa2b0c00255ee3a114cf0d8b52fd
changon <tyu4778@gmail.com>
2024-03-27 00:31:41 +0900

    design: 다크모드 버튼 디자인 변경

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -29,7 +29,7 @@ const ThemeButton = ({}: ThemeButtonProps) => {
   return (
     <button id="theme-toggle" type="button" onClick={toggleTheme} 
     className="text-gray-500 bg-gray-700  dark:text-gray-400 dark:bg-gray-100
-    focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
+    focus:outline-none focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-full text-sm p-2.5">
       {themeIcon ? (
         <svg id="theme-toggle-dark-icon" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
       ) : (
