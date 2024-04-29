5d6bb8a0a4c140d4f1591b22f95129588d1e5a70
YongJun Kwon <dydwns18011@gmail.com>
2024-04-30 01:33:03 +0900

    refactor: api 코드 포맷 변경

--- src/common/api/webtoonAPI.ts
+++ src/common/api/webtoonAPI.ts
@@ -1,9 +1,24 @@
-import { buildUrl } from '../utill/api';
+import { buildUrl } from '@/common/utill/api';
 import axios from 'axios';
 import {
   getWebtoonInfoParam,
   getSearchWebtoonInfoParam,
+  webtoonInfo,
 } from '@/types/webtoon';
+
+const BASE_URL = 'https://korea-webtoon-api.herokuapp.com';
+
+async function fetchAPI<T>(apiUrl: string, params: object): Promise<T> {
+  try {
+    const url = buildUrl(apiUrl, params);
+    const response = await axios.get<T>(url);
+    return response.data;
+  } catch (error) {
+    console.error('API call failed:', error);
+    throw error;
+  }
+}
+
 /**
  * 웹툰 정보 API
  * @param param {Object}
@@ -13,11 +28,8 @@ import {
  * @param param.updateDay {string} 웹툰 업데이트 구분
  * @returns
  */
-
-export const getWebtoonInfo = async (param: getWebtoonInfoParam) => {
-  const url = 'https://korea-webtoon-api.herokuapp.com';
-  const response = await axios.get(buildUrl(url, param));
-  return response.data;
+export const getWebtoonInfo = (params: getWebtoonInfoParam) => {
+  return fetchAPI<webtoonInfo>(BASE_URL, params);
 };
 
 /**
@@ -26,11 +38,6 @@ export const getWebtoonInfo = async (param: getWebtoonInfoParam) => {
  * @param param.keyword {string} 	검색 키워드
  * @returns
  */
-
-export const getSearchWebtoonInfo = async (
-  param: getSearchWebtoonInfoParam,
-) => {
-  const url = 'https://korea-webtoon-api.herokuapp.com/search';
-  const response = await axios.get(buildUrl(url, param));
-  return response.data;
+export const getSearchWebtoonInfo = (params: getSearchWebtoonInfoParam) => {
+  return fetchAPI<webtoonInfo>(`${BASE_URL}/search`, params);
 };
