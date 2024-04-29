7640e305449a98bc0dd999f6dc043b45ab1d7d1c
YongJun Kwon <dydwns18011@gmail.com>
2024-04-30 01:38:01 +0900

    delete: 필요없는 파일 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '@/App';
-import reportWebVitals from '@/reportWebVitals';
 import '@/global.css';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,7 +11,6 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 const queryClient = new QueryClient();
 
-// TODO provider remove
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
@@ -24,8 +22,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
