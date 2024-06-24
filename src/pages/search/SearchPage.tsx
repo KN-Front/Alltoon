5be98767fc9ff742ed4f9b0c0e9a855c3a1c24eb
YongJun Kwon <dydwns18011@gmail.com>
2024-06-24 23:16:24 +0900

    design: 검색 페이지 스크롤 UI 수정

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -5,7 +5,7 @@ import Sidebar from '@/pages/search/components/Sidebar';
 
 const SearchPage = () => {
   return (
-    <div className="lg:px-24 mx-auto justify-center lg:mt-8 pt-10">
+    <div className="lg:px-24 mx-auto justify-center lg:mt-8 pt-10 overflow-hidden">
       <Search />
       <div className="body mt-4 flex flew-row space-x-4">
         <Sidebar />
