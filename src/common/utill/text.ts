5a42b07709bbdcdc0ecf2f7908546e1966194761
YongJun Kwon <dydwns18011@gmail.com>
2024-06-24 23:15:27 +0900

    feat: 문자 자르기 함수 추가

--- src/common/utill/text.ts
+++ src/common/utill/text.ts
@@ -1,3 +1,11 @@
 export const sliceText = (text: string) => {
   return `${text.slice(0, 35)}...`;
 };
+
+export const truncateString = (str: string, num: number) => {
+  if (str.length > num) {
+    return str.slice(0, num) + '...';
+  } else {
+    return str;
+  }
+};
