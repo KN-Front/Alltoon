88af9308c985704c2075336a9fc6eb3ef305a1e6
YongJun Kwon <dydwns18011@gmail.com>
2024-07-12 17:12:44 +0900

    chore: next 14 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,40 @@
 module.exports = {
   env: {
+    browser: true,
     es2021: true,
     node: true,
   },
-  extends: ['prettier'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+    'prettier',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react/no-unescaped-entities': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@next/next/no-img-element': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'prefer-const': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
