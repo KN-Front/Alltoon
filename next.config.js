588e33c309438363ceb242de7235edb7f466860b
changonna <tyu4778@gmail.com>
2024-07-16 02:25:10 +0900

    chore: 배포 폴더 변경
    - out 폴더로 변경
    - clean 후 배포

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  output: 'export',
+  output: 'export', // 정적 사이트로 내보내기 설정
+  cleanDistDir: true, // 빌드 전에 기존 디렉토리 삭제
 };
 
 module.exports = nextConfig;
