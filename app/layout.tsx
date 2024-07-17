05585f3442586fb407c240af6100ce59125e60e0
YongJun Kwon <dydwns18011@gmail.com>
2024-07-17 11:11:34 +0900

    chore: SEO 메타 태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,14 @@ import './global.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import FloatingButtons from './layout/FloatingButtons';
 import { GlobalProvider } from '@/store/GlobalContext';
+import SEO from './layout/SEO';
 
 const queryClient = new QueryClient();
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="kor">
+      <SEO />
       <body className="flex justify-center min-h-screen">
         <div className="w-full max-w-[1430px] mx-auto md:p-4">
           <QueryClientProvider client={queryClient}>
