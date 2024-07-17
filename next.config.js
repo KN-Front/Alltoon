43e20754d50414b292de9b9937e0cf7d26a3b0b9
changonna <tyu4778@gmail.com>
2024-07-17 10:36:42 +0900

    chore: 배포 설정 수정
    - next.config.js 'cleanDistDir' 제거
    - package.json에서 homepage 및 배포 스크립트 수정

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // 정적 사이트로 내보내기 설정
-  cleanDistDir: true, // 빌드 전에 기존 디렉토리 삭제
+  output: 'export',
+  assetPrefix: isProd ? 'https://kn-front.github.io/Alltoon/' : '',
 };
 
 module.exports = nextConfig;
