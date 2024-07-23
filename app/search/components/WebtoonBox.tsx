a8f7a6dc01f8d9e99b76a1135a2d5526b487ceab
changonna <tyu4778@gmail.com>
2024-07-23 16:12:12 +0900

    design: 검색 페이지 list hover 추가
    - hover (light, dark) 추가
    - tailwindcss 사용자 색상 클래스로 변경

--- app/search/components/WebtoonBox.tsx
+++ app/search/components/WebtoonBox.tsx
@@ -7,27 +7,27 @@ interface WebtoonBoxProps {
 
 const WebtoonBox: React.FC<WebtoonBoxProps> = ({ webtoon }) => {
   return (
-    <div className="flex items-center p-2 border border-gray-200 rounded-md shadow-sm">
+    <a
+      href={webtoon.url}
+      className="flex items-center p-2 no-underline transition duration-200 border rounded-md shadow-sm group border-light-bg-secondary dark:border-dark-bg-secondary hover:bg-light-bg-third dark:hover:bg-dark-bg-third hover:border-light-primary dark:hover:border-dark-white"
+      aria-label={`View details for ${webtoon.title}`}
+    >
       <div className="flex-none w-[60px] h-[60px] overflow-hidden rounded-lg">
-        <a href={webtoon.url}>
-          <img
-            className="object-cover w-full h-full"
-            src={webtoon.thumbnail[0]}
-            alt={webtoon.title}
-          />
-        </a>
+        <img
+          className="object-cover w-full h-full"
+          src={webtoon.thumbnail[0]}
+          alt={webtoon.title}
+        />
       </div>
       <div className="flex flex-col ml-4">
-        <div className="text-sm font-bold text-black dark:text-white">
-          <a href={webtoon.url} title={webtoon.title}>
-            {webtoon.title}
-          </a>
+        <div className="text-sm font-bold text-light-text dark:text-dark-white group-hover:text-light-primary dark:group-hover:text-dark-white">
+          {webtoon.title}
         </div>
-        <div className="text-xs text-gray-500 dark:text-gray-400">
+        <div className="text-xs text-light-secondary dark:text-dark-secondary">
           {webtoon.authors.join(', ')}
         </div>
       </div>
-    </div>
+    </a>
   );
 };
 
